dcb3af2e6bf329c1430ca0f999ab4ae11ee4e4a5
Ankit Kalauni <ankit.ka@media.net>
2024-07-22 20:51:54 +0530

    integreted s3 for rw of data

--- README.md
+++ README.md
@@ -23,3 +23,25 @@ change this line 26 and 27 in the file for data range
 end_date = datetime.today()
 start_date = end_date - timedelta(days=2)
 ```
+
+
+
+
+#Steps to follow (raw)
+
+1.setup the nse_data_dumper.py file and check the config.json and output_data/symbol_data.parquet (if there)
+2.change/fix the volumne of the docker-compose file to the proper location of the scripts and data
+3.change/fix the dag script to point to the proper location of python scirpt (location will be the docker location not the local location)
+4.add REQUIREMENT.TXT to the docker-compose file (not recommended but works for small projects)
+5. make .env file and add
+
+```
+AIRFLOW_IMAGE_NAME=apache/airflow:2.4.2
+AIRFLOW_UID=50000
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+AWS_DEFAULT_REGION=ap-south-1
+
+```
+and add this variable to the docker compose file
+6.install boto3 or add to the req file
--- scripts/Yahoo_api/nse_data_dumper.py
+++ scripts/Yahoo_api/nse_data_dumper.py
@@ -1,3 +1,5 @@
+# main.py
+
 import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
@@ -5,17 +7,33 @@ import os
 from datetime import date, datetime
 from pytz import timezone
 import json
+import boto3, io
+from botocore.exceptions import ClientError
+from logger import Logger
 
 class Nse_data_dumper:
     '''
-    To get the data for each valid NSE symbols from given START_TS and END_TS
+    To get the data for each valid NSE symbol from given START_TS and END_TS
     and update the last run value to the config file.
     '''
     
-    def __init__(self) -> None:
+    def __init__(self, logger, log_file_obj) -> None:
+        self.logger = logger
+        self.log_file_obj = log_file_obj
         self.cols = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume','symbol']
         self.new_cols = ['Date', 'Close', 'Volume','symbol']
         self.config_loc = '/opt/airflow/scripts/config.json'
+
+        self.s3_client = boto3.client('s3')
+
+        with open(self.config_loc, 'r') as file_obj:
+            _json_file = json.load(file_obj)
+
+        self.s3bucket_name = _json_file['bucket_name']
+        self.s3object_key = _json_file['object_key']
+        self.last_update = datetime.fromisoformat(_json_file['last_update'])
+
+        del file_obj
 
     def get_valid_symbol(self,read_file_loc='/opt/airflow/datasets/valid_symbols.csv'):    
         '''
@@ -32,13 +50,12 @@ class Nse_data_dumper:
         try:
             symbols_data = pd.read_csv(read_file_loc)
         except:
-            symbols_data = pd.DataFrame([],columns = Nse_data_dumper.new_cols)
+            symbols_data = pd.DataFrame([],columns = self.new_cols)
 
         finally:
             return symbols_data
 
-
-    def DataExtract(self,ticker, start_date, end_date,time_frame = '1h'):
+    def DataExtract(self, ticker, start_date, end_date, time_frame = '1h'):
         '''
         function to call Yahoo API and get the data for {TICKER}
         '''
@@ -48,8 +65,7 @@ class Nse_data_dumper:
         temp.columns = self.cols
         return temp
 
-
-    def update_lastupdate(self,ts,file_loc=None, log_obj=None):
+    def update_lastupdate(self, ts, file_loc=None):
         if not file_loc:
             file_loc = self.config_loc
         with open(file_loc, 'r+') as file_obj:
@@ -58,78 +74,129 @@ class Nse_data_dumper:
             file_obj.seek(0)
             json.dump(json_file, file_obj)
             file_obj.truncate()
-            
-            if log_obj:
-                log_obj.log(f'file saved: {ts}')
+            self.logger.log(self.log_file_obj, f"Last update time saved: {ts}")
 
-
-    def get_lastupdate(self,file_loc=None, log_obj=None):
+    def get_lastupdate(self, file_loc=None):
+        """
+        Instead, use class variable to get the last updated date
+        """
         if not file_loc:
             file_loc = self.config_loc
         with open(file_loc, 'r') as file_obj:
             json_file = json.load(file_obj)
-
             return datetime.fromisoformat(json_file['last_update'])
 
-    def merge_and_export(self,df_input, file_path):
+    def get_S3data(self, file_loc=None):
+        '''
+        Get the parquet data from the S3 location.
+        Setup the config for AWS access key if not getting for boto3.
+        '''
         try:
-            if os.path.exists(file_path):
-                main_df = pd.read_parquet(file_path)
+            s3 = boto3.client('s3')
+            response = s3.get_object(Bucket=self.s3bucket_name, Key=self.s3object_key)
+            parquet_file = io.BytesIO(response['Body'].read())
+            del response, s3
+
+            self.logger.log(self.log_file_obj, f'Loaded data from s3 to the machine')
+
+
+            return pd.read_parquet(parquet_file)
+        except ClientError as e:
+            self.logger.log(self.log_file_obj, f'An error occurred while accessing S3: {e}')
+            raise
+    
+    def _put_S3data(self, df: pd.DataFrame, file_loc=None):
+        '''
+        Input ->
+        file: pandas dataframe
+
+        Stores the pandas dataframe to the S3 bucket named in the config file.
+        '''
+        try:
+            parquet_buffer = io.BytesIO()
+            df.to_parquet(parquet_buffer, index=False, engine='pyarrow', compression='snappy')
+        
+            self.s3_client.put_object(Bucket=self.s3bucket_name, Key=self.s3object_key, Body=parquet_buffer.getvalue())
+            self.logger.log(self.log_file_obj, "Data successfully uploaded to S3.")
+            return True
+        except Exception as e:
+            self.logger.log(self.log_file_obj, f'Error occurred while uploading data to S3: {e}')
+            raise e
+
+    def _check_file_exists(self, bucket_name, object_key):
+        try:
+            self.s3_client.head_object(Bucket=bucket_name, Key=object_key)
+            return True
+        except ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                return False
+            else:
+                # For any other error code, re-raise the exception
+                self.logger.log(self.log_file_obj, f'Error while checking for object in S3: {e}')
+                raise
+
+    def merge_and_export(self, df_input, file_path):
+        try:
+            if self._check_file_exists(bucket_name=self.s3bucket_name, object_key=self.s3object_key):
+                main_df = self.get_S3data()
                 df_merged = pd.concat([main_df, df_input], ignore_index=True)
             else:
                 df_merged = df_input
 
             df_merged = df_merged.drop_duplicates()
             
-            # Filter out data older than 6 months -------------------------------------------------------
             six_months_ago = datetime.now(timezone("Asia/Kolkata")) - timedelta(days=6*30)
             df_merged = df_merged[df_merged['Date'] >= pd.to_datetime(six_months_ago).strftime('%Y-%m-%d %H:%M:%S.%f')]
-            print('doneeeeeeeeeeeeeee')
-            df_merged.to_parquet(file_path, compression='snappy')
-            
-            print(f"Data merged, cleaned, and exported successfully to {file_path}")
-            
-        except Exception as e:
-            print(f"An error occurred: {e}")
 
+            if self._put_S3data(df_merged):
+                self.logger.log(self.log_file_obj, f"Data merged, cleaned, and exported successfully to {file_path}")
+            else:
+                raise Exception('An error occurred while uploading the new object to S3')
+        except Exception as e:
+            self.logger.log(self.log_file_obj, f"An error occurred: {e}")
+            raise
 
 if __name__ == '__main__':
-    nse = Nse_data_dumper()
+    # Initialize the logger
+    logger = Logger()
 
-    start_date = nse.get_lastupdate()
-    end_date = datetime.today()
+    # Specify the log file path
+    log_file_path = '/opt/airflow/logs/activity_log.txt'
 
+    # Open the log file in append mode
+    with open(log_file_path, 'a') as log_file_obj:
+        nse = Nse_data_dumper(logger, log_file_obj)
 
-    if start_date.date() == end_date.date():
-        exit(69)
-    print(f'starting session for {start_date} to {end_date}')
+        start_date = nse.last_update
+        end_date = datetime.today()
 
-    symbols = nse.get_valid_symbol()
-    
-    temp_df = pd.DataFrame([],columns=nse.cols)
+        if start_date.date() == end_date.date():
+            logger.log(log_file_obj, f"No data to update. Start and end dates are the same: {start_date.date()}")
+            exit(69)
+        logger.log(log_file_obj, f"Starting session for {start_date} to {end_date}")
 
-    for i, symbol in enumerate(symbols):
-    # exit(1)
-        try:
-            temp_df = pd.concat([temp_df, nse.DataExtract(symbol+'.NS', start_date, end_date)], ignore_index=1)
-            print(f'after running {symbol} here is the updated shape {temp_df.shape}')
-            print(f'{i}. {symbol} iteration done\n')
-            i+=1
-        except Exception as e:
-            print(e)
-            print(f'{i}. {symbol} iteration Failed\n')
-            i+=1    
-
-    temp_df = temp_df[nse.new_cols]
-    temp_df['Close'] = temp_df['Close'].astype(int)
-    temp_df['Date'] = pd.to_datetime(temp_df['Date'], errors='coerce')
-
-
-    print('after running all symbols here is the dim of df', temp_df.shape)
-    try:
-        nse.merge_and_export(temp_df, '../all_symbols_data.parquet')
-        nse.update_lastupdate(end_date)
-    except:
-        print('new feather file creation failed')
+        symbols = nse.get_valid_symbol()
         
+        temp_df = pd.DataFrame([], columns=nse.cols)
 
+        for i, symbol in enumerate(symbols[:5]):
+            try:
+                temp_df = pd.concat([temp_df, nse.DataExtract(symbol+'.NS', start_date, end_date)], ignore_index=1)
+
+                if i%100 == 0:
+                    logger.log(log_file_obj, f"After running {symbol}, updated shape: {temp_df.shape}")
+                    logger.log(log_file_obj, f"{i}. {symbol} iteration done")
+            except Exception as e:
+                logger.log(log_file_obj, f"{i}. {symbol} iteration failed: {e}")
+        
+        temp_df = temp_df[nse.new_cols]
+        temp_df['Close'] = temp_df['Close'].astype(int)
+        temp_df['Date'] = pd.to_datetime(temp_df['Date'], errors='coerce')
+
+        logger.log(log_file_obj, f"After running all symbols, dataframe shape: {temp_df.shape}")
+
+        try:
+            nse.merge_and_export(temp_df, '../all_symbols_data.parquet')
+            nse.update_lastupdate(end_date)
+        except Exception as e:
+            logger.log(log_file_obj, f"New Parquet file creation failed: {e}")
